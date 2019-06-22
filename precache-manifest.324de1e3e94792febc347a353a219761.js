e2535ff5e12426cefc190f23bc0e9fbc90cfbaf5
istanisic <ivanstanisic1@gmail.com>
2019-06-22 11:19:58 -0400

    Updates

---
+++ precache-manifest.324de1e3e94792febc347a353a219761.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7edf47cd2060d5e6261682a5a8c735ea",
+    "url": "/fuze-health-tracker/index.html"
+  },
+  {
+    "revision": "4b54194d90d723a30a63",
+    "url": "/fuze-health-tracker/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/fuze-health-tracker/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "4b54194d90d723a30a63",
+    "url": "/fuze-health-tracker/static/js/main.9e6e0289.chunk.js"
+  },
+  {
+    "revision": "8fa626278ea79a41cdd6",
+    "url": "/fuze-health-tracker/static/js/runtime~main.fd0c0d21.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fuze-health-tracker/static/media/logo.5d5d9eef.svg"
+  }
+]);
